65851d85bfef559ed90818186f2989ec9e3ec422
NedalAdham <75937336+NedalAdham@users.noreply.github.com>
2022-05-25 10:37:32 +0200

    Add files via upload

---
+++ CTCI-LEET/Linked Lists/142. Linked List Cycle II.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        f , s = head , head
+        while f and f.next : 
+            f=f.next.next
+            s = s.next
+            if f == s :
+                s = head 
+                while s != f:
+                    s = s.next
+                    f=f.next
+                return s
+            
+        return None
+        
