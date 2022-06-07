37c45e48a56077b010eb4bf03efc65395df2412f
NedalAdham <75937336+NedalAdham@users.noreply.github.com>
2022-06-07 18:31:45 +0200

    Add files via upload

---
+++ CTCI-LEET/june/52. N-Queens II.py
@@ -0,0 +1,34 @@
+
+        
+class Solution:
+    def totalNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posdio = set() #(r+c)
+        negdio = set() #(r-c)
+
+        res = []
+        board = [["."] * n for i in range (n)]
+        def backtrack (r):#row by row base case
+            if r == n :
+                copy = ["".join (row)for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r+c) in posdio or (r-c) in negdio:
+                    continue
+                col.add(c)
+                posdio.add(r + c)
+                negdio.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r+1)
+
+                #backtracking undo
+                col.remove(c)
+                posdio.remove(r + c)
+                negdio.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return len(res)
